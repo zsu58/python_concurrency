15a435e1a5090b3b4e3aa7afd9b4b175cb63f5e5
zsu58 <zsu58@icloud.com>
2023-11-09 00:34:57 +0900

    feat: chain asynchronous

---
+++ asynchronous/chain.py
@@ -0,0 +1,54 @@
+import asyncio
+import random
+import time
+
+async def part1(n: int) -> str:
+  i = random.randint(0, 10)
+  print(f"part1({n}) sleeping for {i} seconds.")
+  await asyncio.sleep(i)
+  result = f"result{n}-1"
+  print(f"Returning part1({n}) == {result}.")
+  return result
+
+async def part2(n: int, arg: str) -> str:
+  i = random.randint(0, 10)
+  print(f"part2{n, arg} sleeping for {i} seconds.")
+  await asyncio.sleep(i)
+  result = f"result{n}-2 derived from {arg}"
+  print(f"Returning part2{n, arg} == {result}.")
+  return result
+
+async def chain(n: int) -> None:
+  start = time.perf_counter()
+  p1 = await part1(n)
+  p2 = await part2(n, p1)
+  end = time.perf_counter() - start
+  print(f"-->Chained result{n} => {p2} (took {end:0.2f} seconds).")
+
+async def main(*args):
+  await asyncio.gather(*(chain(n) for n in args))
+
+if __name__ == "__main__":
+  import sys
+  random.seed(444)
+  args = [1, 2, 3] if len(sys.argv) == 1 else map(int, sys.argv[1:])
+  start = time.perf_counter()
+  asyncio.run(main(*args))
+  end = time.perf_counter() - start
+  print(f"Program finished in {end:0.2f} seconds.")
+  # part1(1) sleeping for 4 seconds.
+  # part1(2) sleeping for 4 seconds.
+  # part1(3) sleeping for 0 seconds.
+  # Returning part1(3) == result3-1.
+  # part2(3, 'result3-1') sleeping for 4 seconds.
+  # Returning part1(1) == result1-1.
+  # part2(1, 'result1-1') sleeping for 7 seconds.
+  # Returning part1(2) == result2-1.
+  # part2(2, 'result2-1') sleeping for 4 seconds.
+  # Returning part2(3, 'result3-1') == result3-2 derived from result3-1.
+  # -->Chained result3 => result3-2 derived from result3-1 (took 4.00 seconds).
+  # Returning part2(2, 'result2-1') == result2-2 derived from result2-1.
+  # -->Chained result2 => result2-2 derived from result2-1 (took 8.00 seconds).
+  # Returning part2(1, 'result1-1') == result1-2 derived from result1-1.
+  # -->Chained result1 => result1-2 derived from result1-1 (took 11.00 seconds).
+  # Program finished in 11.00 seconds.
